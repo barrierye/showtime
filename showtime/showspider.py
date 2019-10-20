d00b2ef1749fb0ecf946445af81cbade3b41f294
barrierye <barriery@qq.com>
2019-10-21 00:47:42 +0800

    add some commits

--- showtime/showspider.py
+++ showtime/showspider.py
@@ -99,16 +99,53 @@ class ChinaTicket(Spider):
         return detailed_infos
 
 class Show(list):
+    """存储具体单个show的所有场次的类
+
+    Attributes:
+        name: show的名称
+        url: show具体介绍的url
+    
+    Elements in self:
+        至少包含'day_date', 'time_date', 'url', 'province', 'place',
+        'in_sale_price', 'sold_out_price'字段的dict
+    """
     def __check_params(self, requested_params, params):
         for param in requested_params:
             if params.get(param) is None:
                 raise Exception('%s is requested, but not int detailed_info', param)
     def __init__(self, params):
+        """类Show的构造函数
+
+        Args:
+            params: 至少包含'name'和'url'的dict，其中，
+                        name - show的名字
+                        url - show具体介绍的url
+
+        Raises:
+            Exception: 当params中不存在'name'或'url'时会抛出异常
+        """
         super(Show, self).__init__()
         self.__check_params(['name', 'url'], params)
         for attr, value in params.items():
             setattr(self, attr, value)
     def add_event(self, detailed_info):
+        """向Show中添加一个场次
+
+        Args:
+            detailed_info: 至少包含'day_date', 'time_date', 'url', 'province',
+                           'place', 'in_sale_price', 'sold_out_price'，其中，
+                                day_date - 该场具体日期
+                                time_date - 该场当天具体时间
+                                url - 该场购票详细地址
+                                province - 该场所在省份
+                                place - 该场具体位置
+                                in_sale_price - 仍在销售中的价格
+                                sold_out_price - 已经卖完的价格
+
+        Raises:
+            Exception: 当params中不存在'day_date', 'time_date', 'url', 'province',
+                       'place', 'in_sale_price', 'sold_out_price'时会抛出异常
+        """
         self.__check_params(['day_date', 'time_date', 'url', 'province', 'place', 'in_sale_price', 'sold_out_price'], detailed_info)
         self.append(detailed_info)
     def add_events(self, detailed_infos):
