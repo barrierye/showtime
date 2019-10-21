31d55812f77817d37ca2897ddd1a4e0aa55357d8
barrierye <barriery@qq.com>
2019-10-21 20:10:03 +0800

    add commit

--- showtime/__init__.py
+++ showtime/__init__.py
@@ -2,4 +2,6 @@ import os
 import platform
 
 if platform.system() == 'Darwin':
+    # This fix doesn't work on Mojave. It does the trick on High Sierra though.
+    # see: https://github.com/darkskyapp/forecast-ruby/issues/13
     os.environ['OBJC_DISABLE_INITIALIZE_FORK_SAFETY'] = 'YES'
